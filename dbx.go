656f2a188fc957a5448a92520e32de5c5d0f5b36
Oleg Kovalov <hey@olegk.dev>
2025-08-25 20:09:01 +0200

    Initial implementation

---
+++ dbx.go
@@ -0,0 +1,90 @@
+package dbx
+
+import (
+	"context"
+)
+
+// Client ...
+type Client interface {
+	Conn
+
+	// Acquire ...
+	Acquire(ctx context.Context) (Conn, error)
+
+	// Release ...
+	Release(Conn)
+}
+
+// Conn ...
+type Conn interface {
+	// Ping ...
+	Ping(ctx context.Context) error
+
+	// BeginTx ...
+	BeginTx(ctx context.Context, opts TxOptions) (Tx, error)
+
+	// Exec ...
+	Exec(ctx context.Context, query string, args ...any) (Result, error)
+
+	// Query ...
+	Query(ctx context.Context, query string, args ...any) (Rows, error)
+
+	// QueryRow ...
+	QueryRow(ctx context.Context, query string, args ...any) Row
+
+	// Close ...
+	Close() error
+}
+
+// Result ...
+type Result interface {
+	// LastInsertId ...
+	LastInsertId() (int64, error)
+
+	// RowsAffected ...
+	RowsAffected() (int64, error)
+}
+
+// Rows ...
+type Rows interface {
+	Row
+
+	// Next ...
+	Next() bool
+
+	// Close ...
+	Close() error
+
+	// Err ...
+	Err() error
+}
+
+// Row ...
+type Row interface {
+	// Scan ...
+	Scan(dst ...any) error
+}
+
+// Tx ...
+type Tx interface {
+	// Begin ...
+	Begin(ctx context.Context) (Tx, error)
+
+	// Commit ...
+	Commit(ctx context.Context) error
+
+	// Rollback ...
+	Rollback(ctx context.Context) error
+
+	// Exec ...
+	Exec(ctx context.Context, query string, args ...any) ([]byte, error)
+
+	// Query ...
+	Query(ctx context.Context, query string, args ...any) (Rows, error)
+
+	// QueryRow ...
+	QueryRow(ctx context.Context, query string, args ...any) Row
+}
+
+// TxOptions ...
+type TxOptions struct{}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cristalhq/dbx
+
+go 1.24
